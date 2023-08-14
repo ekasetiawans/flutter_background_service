a04d3a75ca0a8e4683802b0a01e41b0dd50ba37b
Saeed Ahmadi <46680607+codingwithsaeed@users.noreply.github.com>
2023-08-14 11:04:53 +0330

    Fix: service do not connect to dart side after destroy by xiaomi boost memory.

--- packages/flutter_background_service_android/android/src/main/java/id/flutter/flutter_background_service/BackgroundService.java
+++ packages/flutter_background_service_android/android/src/main/java/id/flutter/flutter_background_service/BackgroundService.java
@@ -101,6 +101,7 @@ public class BackgroundService extends Service implements MethodChannel.MethodCa
         notificationContent = config.getInitialNotificationContent();
         notificationId = config.getForegroundNotificationId();
         updateNotificationInfo();
+        onStartCommand(null, -1, -1);
     }
 
     @Override
